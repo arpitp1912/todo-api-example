7d49a163f47efaa8c02a57d7e17c6af2686aa937
Arpit Shah <arpitp1912@gmail.com>
2017-07-04 01:38:05 +0530

    Update PUT /todo to sequelize

--- server.js
+++ server.js
@@ -67,20 +67,6 @@ app.post('/todos', function(req, res){
 
 app.delete('/todos/:id', function(req, res){
     var todoid = parseInt(req.params.id, 10);
-    
-    // var matchedTodo = _.findWhere(todos, {id: todoid});
-
-    // if(!matchedTodo) {
-    //     return res.status(404).json({"error":"No task found with given id"});
-    // }
-
-    // console.log("Removing Task: " + matchedTodo.description)
-
-    // todos = _.without(todos, matchedTodo);
-
-    // console.log("Removed Task: " + matchedTodo.description);
-
-    // res.json(todos);
 
     db.todo.destroy({
         where: {
@@ -102,33 +88,34 @@ app.delete('/todos/:id', function(req, res){
 
 app.put('/todos/:id', function(req, res){
     var todoid = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoid});
-
-    if(!matchedTodo) {
-        return res.status(404).json({"error":"No task found with given id"});
-    }
     
     var body = _.pick(req.body, 'description', 'completed');
 
-    var validAttritubes = {};
+    var attritubes = {};
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-        validAttritubes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')){
-        return res.status(400).send();
+    if (body.hasOwnProperty('completed')){
+        attritubes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
-        body.description = body.description.trim();
-        validAttritubes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('description')){
+        attritubes.description = body.description;
     }
 
-    _.extend(matchedTodo, body);
-
-    res.json(todos)
-
+    db.todo.findById(todoid).then(function(todo){
+        if (todo){
+            return todo.update(attritubes).then(function(todo){
+            res.json(todo.toJSON());
+    }, function(e){
+        res.status(400).json(e);
+    })
+        } else {
+            res.status(404).json({
+                    "error": "Unable to find task with ID"
+                });
+        }
+    }, function(){
+        res.status(500).send()
+    })
 })
 
 
